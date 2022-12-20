096535c6a93b60018db8347dfd743bd07403da71
MaheshChennaboina <65758531+MaheshChennaboina@users.noreply.github.com>
2022-12-20 22:55:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors(
     {
-        origin:["http://localhost:3000"],
+        origin:["https://yogamantra.netlify.app/"],
         methods:["GET","POST"],
         credentials:true
     }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(session(
